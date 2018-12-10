fa7ce810911338652887bb39daefa9a96a52ef2c
Sam Ong <samong@Sams-MacBook-Pro.local>
2018-12-11 11:40:53 +1300

    Added basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -20,11 +27,6 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-
-  def index
-    @articles = Article.all
-  end
-
 
   def update
     @article = Article.find(params[:id])
